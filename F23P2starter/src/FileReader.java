f225f26b4d4afa19abddb453167c80ac51dc8369
Jae Trimboli <jaetrim@vt.edu>
2023-09-19 16:33:01 -0400

    seminarDB

--- F23P2starter/src/FileReader.java
+++ F23P2starter/src/FileReader.java
@@ -3,38 +3,43 @@ import java.util.Scanner;
 
 /**
  * FileReader Class
+ * 
  * @author Jae Trimboli (jaetrim)
  * @author Mohammad Mian (mohammadm21)
  * @version 2023-19-09
  */
 
 public class FileReader {
-    
+
+    private SeminarDB world;
+
     /**
      * Constructor for FileReader
+     * 
      * @param size of world
      * @param file for name of file
      * @throws Exception
      */
-    public FileReader(int size, String file) throws Exception{
-        SeminarDB world = new SeminarDB(size, file);
+    public FileReader(int size, String file) throws Exception {
+        world = new SeminarDB(size, file);
         readFile(file);
     }
 
     /**
      * Reads the input file
+     * 
      * @param name of file
      * @throws Exception
      */
     public void readFile(String name) throws Exception {
         File file = new File(name);
         Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()){
+        while (scanner.hasNextLine()) {
             String currLine = scanner.nextLine();
             currLine = currLine.trim();
-            if (currLine.contains("insert")){
+            if (currLine.contains("insert")) {
                 String[] insertLine = currLine.split("\\s+");
-                if (insertLine.length == 2){
+                if (insertLine.length == 2) {
                     char[] charArray = insertLine[1].toCharArray();
                     boolean found = false;
                     for (int i = 0; i < charArray.length; i++) {
@@ -42,7 +47,7 @@ public class FileReader {
                             found = true;
                         }
                     }
-                    if (found == false){
+                    if (found == false) {
                         int id = Integer.parseInt(insertLine[1]);
                         String title = scanner.nextLine().trim();
                         String date = scanner.next().trim();
@@ -56,58 +61,56 @@ public class FileReader {
                         String description = scanner.nextLine();
                         String formatDesc = description.trim();
                         Seminar sem = new Seminar(id, title, date, length,
-                            xCoord, yCoord, cost, keywords, formatDesc);
-                    }
-                    else {
+                                xCoord, yCoord, cost, keywords, formatDesc);
+                        world.insert(sem);
+                    } else {
                         scanner.nextLine();
                         scanner.nextLine();
                         scanner.nextLine();
                         scanner.nextLine();
                     }
-                }
-                else {
+                } else {
                     scanner.nextLine();
                     scanner.nextLine();
                     scanner.nextLine();
                     scanner.nextLine();
                 }
-            }
-            else if (currLine.contains("search")) {
+            } else if (currLine.contains("search")) {
                 String[] searchLine = currLine.split("\\s+");
-                if (searchLine[1] == "location"){
+                if (searchLine[1] == "location") {
                     int x = Integer.parseInt(searchLine[2]);
                     int y = Integer.parseInt(searchLine[3]);
                     int radius = Integer.parseInt(searchLine[4]);
-                }
-                else if (searchLine[1] == "keyword"){
+                    world.searchLocation(x, y, radius);
+                } else if (searchLine[1] == "keyword") {
                     String word = searchLine[2];
-                }
-                else if (searchLine[1] == "ID" && searchLine[1] != null){
+                    world.searchKeyword(word);
+                } else if (searchLine[1] == "ID" && searchLine[1] != null) {
                     int id = Integer.parseInt(searchLine[2]);
-                }
-                else if (searchLine[1] == "cost"){
+                    world.searchID(id);
+                } else if (searchLine[1] == "cost") {
                     int low = Integer.parseInt(searchLine[2]);
                     int high = Integer.parseInt(searchLine[3]);
-                }
-                else if (searchLine[1] == "date"){
-                    int dateTime1 = Integer.parseInt(searchLine[2]);
-                    int dateTime2 = Integer.parseInt(searchLine[3]);
+                    world.searchCost(low, high);
+                } else if (searchLine[1] == "date") {
+                    String dateTime1 = searchLine[2];
+                    String dateTime2 = searchLine[3];
+                    world.searchDate(dateTime1, dateTime2);
                 }
 
-            }
-            else if (currLine.contains("print")) {
+            } else if (currLine.contains("print")) {
                 String[] printLine = currLine.split("\\s+");
-                String print = printLine[1];
+                String printCommand = printLine[1];
+                world.print(printCommand);
 
-            }
-            else if (currLine.contains("delete")) {
+            } else if (currLine.contains("delete")) {
                 String[] deleteLine = currLine.split("\\s+");
                 int id = Integer.parseInt(deleteLine[1]);
-                
+                world.delete(id);
 
             }
         }
         scanner.close();
-        
+
     }
 }
--- F23P2starter/src/SeminarDB.java
+++ F23P2starter/src/SeminarDB.java
@@ -1,18 +1,99 @@
 
 /**
- * World Class 
+ * World Class
+ * 
  * @author Jae Trimboli (jaetrim)
  * @author Mohammad Mian (mohammadm21)
  * @version 2023-19-09
  */
 public class SeminarDB {
-    
+
     /**
      * Seminar DB Constructor
+     * 
      * @param size inputted world size
      * @param name of file
      */
-    public SeminarDB(int size, String name){
+    public SeminarDB(int size, String name) {
+
+    }
+
+    /**
+     * Inseets seminar object into each of the trees
+     * 
+     * @param sem representing Seminar
+     */
+    public void insert(Seminar sem) {
+
+    }
+
+    /**
+     * Search function for printing the record that matches the given ID
+     * 
+     * @param id is the id of record
+     */
+    public void searchID(int id) {
+
+    }
+
+    /**
+     * Search function that prints all records that fall within the range
+     * 
+     * @param low  is low cost
+     * @param high is high cost
+     */
+    public void searchCost(int low, int high) {
+
+    }
+
+    /**
+     * Search function for printing all records that fall within a range of dates /
+     * times
+     * 
+     * @param date1 first date or time
+     * @param date2 second date or time
+     */
+    public void searchDate(String date1, String date2) {
+
+    }
+
+    /**
+     * Search function that prints all records that match the keyword
+     * 
+     * @param word for keyword
+     */
+    public void searchKeyword(String word) {
+
+    }
+
+    /**
+     * Search function that prints all records that fall within radius distance of
+     * the search point
+     * 
+     * @param xCoor is X coordinate
+     * @param yCoor is Y coordinate
+     * @param rad   is the radius
+     */
+    public void searchLocation(int xCoor, int yCoor, int rad) {
+
+    }
+
+    /**
+     * Deletes a record with the given ID from ALL search trees
+     * 
+     * @param id of record
+     */
+    public void delete(int id) {
+
+    }
+
+    /**
+     * Depending on which print command is passed, print out a traversal of the
+     * corresponding tree
+     * 
+     * @param command
+     */
+    public void print(String command) {
 
     }
 }
